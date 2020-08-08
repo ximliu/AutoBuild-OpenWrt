c766f9090d4f3b2e438d9b286e91abefcb17ce61
ywgf88 <43352863+ywgf88@users.noreply.github.com>
2020-08-08 10:38:08 +0800

    Update customize.sh

--- customize.sh
+++ customize.sh
@@ -8,4 +8,4 @@
 #1. Modify default IP
 sed -i 's/192.168.1.1/192.168.5.1/g' openwrt/package/base-files/files/bin/config_generate
 # add passwall
-sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
+#sed -i '$a src-git lienol https://github.com/Lienol/openwrt-package' feeds.conf.default
